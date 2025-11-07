1cc6b90cad6cd307a152da067ec7955cb2b5ac8a
LeeelaLi <anotherlees@outlook.com>
2025-11-07 11:09:57 +0000

    Initial commit

---
+++ socially/src/app/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import { getPosts } from "@/actions/post.action";
+import WhoToFollow from "@/components/WhoToFollow";
+import PostCard from "@/components/PostCard";
+
+export default async function Home() {
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
+  console.log({ posts });
+return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
+    </div>
+  );
+}
